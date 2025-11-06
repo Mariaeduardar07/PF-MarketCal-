04485fd3cad245910ecec46bd8d59bf39acb1008
anna <acfiochi@gmail.com>
2025-11-06 09:22:13 -0300

    feat: create header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Header from "../components/Header";
+import Header from "@/components/Header";
+import SideHeader from "@/components/SideHeader";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <SideHeader />
     </div>
   );
 }
