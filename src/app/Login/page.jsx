cb74ad4bd4856e912b3ffa729aa4b89e3db14070
Maria Eduarda <mariaeduardatds2.senai@gmail.com>
2025-11-06 11:19:59 -0300

    Feature: Implement layout and styles for the form section of the login page

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./login.module.css";
+import Image from "next/image";
 
 export default function Login() {
   return (
@@ -12,6 +13,45 @@ export default function Login() {
             Sua estratégia de social media, organizada para performar
           </p>
         </div>
+      </div>
+
+      {/* DIREITA */}
+      <div className={styles.right}>
+        <Image
+          src="/image/logo.png"
+          alt="Logo do MarketCal"
+          width={215}
+          height={215}
+          className={styles.logoImg}
+          priority
+        />
+        <form className={styles.form}>
+          <div className={styles.formGroup}>
+            <label htmlFor="email" className={styles.label}>
+              E-mail
+            </label>
+            <input
+              type="email"
+              id="email"
+              className={styles.input}
+              placeholder="Digite seu e-mail"
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="password" className={styles.label}>
+              Senha
+            </label>
+            <input
+              type="password"
+              id="password"
+              className={styles.input}
+              placeholder="Digite sua senha"
+            />
+          </div>
+          <button type="submit" className={styles.button}>
+            Entrar
+          </button>
+        </form>
       </div>
     </div>
   );
