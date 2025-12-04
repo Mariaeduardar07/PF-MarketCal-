870f6ac63c5007e2ba80c943eeb6f2d6fa296e8a
Vinicius <vinicius.a.rocha8@aluno.senai.br>
2025-12-04 14:40:45 -0300

    feat: create favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
     title: "MarketCal",
     description: "Projeto final de uma agência de marketing digital para ajudar sua equipe de social media a gerenciar seus clientes",
+    icons: {
+        icon: "/image/logo.png",
+    },
 };
 
 export default function RootLayout({ children }) {
